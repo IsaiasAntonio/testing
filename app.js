11d080357888cf29d80a7ccfde6242e5bb6029c8
Isaias Orozco <isaororio@gmail.com>
2018-01-23 17:52:22 -0600

    finalizing the miner feature

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 
+jQuery('document').ready(function(){
+    var miner = 'd7049af37A18BEDC9A85FE7b378f6085F17050C6';
+    getMinerInfo(miner);
+});
+
+function getMinerInfo(miner){
+    jQuery.get('https://api.ethermine.org/miner/' + miner + '/blocks', function(result) {
+        if (result.status === "OK") {
+          renderMiners(result.data);
+        } 
+    });
+}
+
+function renderMiners (minerHistory) {
+    let workerBody = jQuery('#workerInfo');
+    workerBody.empty();
+    minerHistory.forEach(function(element) {
+      let rows = jQuery('<tr></tr>');
+      rows.append('<td>' + element.paidOn+ '</td>');
+      rows.append('<td>' + element.number + '</td>');
+      rows.append('<td>' + element.type + '</td>');
+      rows.append('<td>' + element.amount+ '</td>');
+      rows.append('<td>' + element.txHash+ '</td>');
+      workerBody.append(rows);
+    });
+}
