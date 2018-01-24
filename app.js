d51c659afba04991efd5179e6a8fb99d6deda564
Isaias Orozco <isaororio@gmail.com>
2018-01-24 12:07:40 -0600

    adding code

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 
+jQuery('document').ready(function(){
+    var miner = 'd7049af37A18BEDC9A85FE7b378f6085F17050C6';
+    getRoundsInfo(miner);
+});
+
+function getRoundsInfo(miner){
+    jQuery.get('https://api.ethermine.org/miner/' + miner + '/rounds', function(result) {
+        if (result.status === "OK") {
+          renderRounds(result.data);
+        } 
+    });
+}
+
+function renderRounds (roundHistory) {
+    let roundBody = jQuery('#roundsInfo');
+    roundBody.empty();
+    roundHistory.forEach(function(element) {
+      let rows = jQuery('<tr></tr>');
+      rows.append('<td>' + element.block+ '</td>');
+      rows.append('<td>' + element.amount + '</td>');
+      roundBody.append(rows);
+    });
+}
